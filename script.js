e183ed5f1618f8f7452149fbd53ebdc4a9e9a3f8
Étienne Charron <93718161+etennecharron@users.noreply.github.com>
2024-07-08 23:56:37 -0400

    finalisation oeuvre en detail
    j'ai terminer l'oeuvre en détail format ordi, il reste juste a le faire fonctionner format téléphone

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ let btnProjets = document.querySelector(".btnProjets");
 
 let titre = document.querySelector(".titre");
 
+let oeuvresArr = document.querySelectorAll(".swiperOeuvres .swiper-slide img")
+
+/*****modifie la position des oeuvre sur la position des Y**********/
+let toggleHautBas = false;
+oeuvresArr.forEach((oeuvre)=>{
+    if(toggleHautBas == false){
+        toggleHautBas = true;
+        let nbRdm = Math.round(Math.random()*40);
+        console.log(nbRdm)
+        oeuvre.style.marginBottom = nbRdm + "px";
+    }
+    else if(toggleHautBas == true){
+        toggleHautBas = false;
+        let nbRdm = Math.round(Math.random()*-150);
+        console.log(nbRdm)
+        oeuvre.style.marginBottom = nbRdm + "px";
+    }
+
+});
 let contenus = {
     accueil: {
         active : true,
@@ -44,10 +63,9 @@ btnAccueil.addEventListener("click",function(){
 
 /********DÉBUT SWIPER GAMING BABY**********/ 
 
-const swiper = new Swiper(".swiper", {
-    
+const swiperOeuvres = new Swiper(".swiperOeuvres", {
     slideToClickedSlide: true,
-    slidesPerView: "2",
+    slidesPerView: "3",
     freeMode: {
         enabled: true,
         sticky: false,
@@ -55,7 +73,7 @@ const swiper = new Swiper(".swiper", {
         },
     mousewheel: {
         enabled: true,
-        sensitivity: 4,
+        sensitivity: 10,
         },
     scrollbar: {
         el: '.swiper-scrollbar',
@@ -65,7 +83,15 @@ const swiper = new Swiper(".swiper", {
 })
 
 
-
-
-
+const swiperImages = new Swiper(".swiperImages",{
+slidesPerView:1,
+loop:true,
+pagination: {
+    el: '.swiper-pagination',
+},
+navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+},
+})
 /********FIN SWIPER GAMING BABY**********/ 
