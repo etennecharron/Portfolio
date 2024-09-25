25fb5a62bb943add5fa878500a1eda63b5235251
Étienne Charron <93718161+etennecharron@users.noreply.github.com>
2024-09-25 17:38:32 -0400

    correction + projet vers accueil
    maintenant l'utilisateur peux retourner à l'accueil en cliquant sur mon nom et j'ai corriger quelque textes qu'il y avais dans mes oeuvres

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let btnProjets = document.querySelector(".btnProjets01");
 let imgScroll = document.querySelector(".imgScroll");
 let imgSwipe = document.querySelector(".imgSwipe");
 
+let btnNom = document.querySelector(".titreNom");
+let btnPortfolio = document.querySelector(".titrePortfolio")
+
 let titre = document.querySelector(".titre");
 let contenusPage = document.querySelector(".contenusPage");
 let oeuvresArr;
@@ -11,12 +14,60 @@ let btnFermerOeuvre = document.querySelector(".oeuvreWrapup__contenus__ecran__x"
 let oeuvreOuvert = document.querySelector(".oeuvreWrapup");
 let carousselOeuvres = document.querySelector(".swiperOeuvres");
 
+/******* swiper pour les oeuvres**********/
+const swiperOeuvres = new Swiper(".swiperOeuvres", {
+    slidesPerView: 1,
+    breakpoints: {
+        1300: {
+            slidesPerView: 3,
+            mousewheel: {
+                enabled: true,
+                sensitivity: 5,
+            },
+        },
+        830: {
+            slidesPerView: 2,
+            mousewheel: {
+                enabled: true,
+                sensitivity: 3,
+            },
+        },
+        550: {
+            slidesPerView: 1,
+            mousewheel: {
+                enabled: true,
+                sensitivity: 2,
+            },
+        }
+
+    },
+    freeMode: {
+        enabled: true,
+        sticky: false,
+        momentumBounce: false,
+    },
+    mousewheel: {
+        enabled: true,
+        sensitivity: 1,
+    },
+})
+
+/********swiper pour les images de l'oeuvre**********/
+const swiperImages = new Swiper(".swiperImages", {
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
+
+
 let oeuvres = [
     {
         titre: "BIOTA",
         imgPresentation: "images/projet01_biota.jpg",
         couleur: "pink",
-        description: "BITOA est un projet réalisé dans le cadre d'un cours d'animation 3D et de conception sonore. L'histoire d'une créature a une forme humanoïde triste et seule. Sa tristesse fait naître la vie et redonne de la couleur à son monde. J'ai réalisé le montage vidéo sur Davinci Resolve, créé les sons sur VCV Rack et réalisé la création 3D du personnage et son animation sur MAYA.",
+        description: "BITOA est un projet réalisé dans le cadre d'un cours d'animation 3D et de conception sonore. C’est l'histoire d'une créature ayant une forme humanoïde. Elle est triste et seule. Sa tristesse fait naître la vie et redonne de la couleur à son monde. J'ai réalisé le montage vidéo sur Davinci Resolve, créé les sons sur VCV Rack et réalisé la création 3D du personnage et son animation sur MAYA..",
         logiciels: ["Unity", "Davinci Resolve", "Maya", "Repear"],
         iframe: [`<iframe src="https://www.youtube.com/embed/TYIZgZ5-SGg?si=Dvpai46RyXvKf_67" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`],
         images: ["images/projet01_biota_contenus01.jpg",
@@ -26,20 +77,29 @@ let oeuvres = [
         titre: "LA GROTTE",
         imgPresentation: "images/projet02_grotte.jpg",
         couleur: "orange",
-        description: "LA GROTTE est un jeu d'évasion où le but est de s'échapper de la grotte où l'on est enfermé grace à la réussite d'énigmes. Les énigmes étaient réalisables grâce à une boîte orange imprimée en 3D qui contenait des composantes comme un détecteur de lumière, 3 boutons, un potentiomètre et un détecteur de temps de vol. Dans le projet, j'ai réaliser avec l'aide de mes collegues le design du jeu et le fonctionnement des énigmes. De plus, j'ai réaliser la programation complète du jeu sur unity et Arduino ide.",
-        logiciels: ["Unity", "Arduino ide", "Max", "VCV Rack", "Adobe Illustrator", "Maya"],
+        description: "LA GROTTE est un jeu d'évasion où le but est de s'échapper de la grotte où on est enfermés grâce à la réussite d'énigmes. Les énigmes sont réalisables grâce à une boîte orange imprimée en 3D qui contient des composantes comme un détecteur de lumière, 3 boutons, un potentiomètre et un détecteur de temps de vol. Dans le projet, j'ai conçu avec l'aide de mes collègues le design du jeu et le fonctionnement des énigmes. De plus, j'ai réalisé la programmation complète du jeu sur Unity et Arduino IDE.",
+        logiciels: ["Unity", "Arduino IDE", "Max", "VCV Rack", "Adobe Illustrator", "Maya"],
         iframe: [],
         images: ["images/projet02_grotte_contenus01.jpg", "images/projet02_grotte_contenus02.jpg", "images/projet02_grotte_contenus03.jpg", "images/projet02_grotte_contenus04.png"],
     }, {
         titre: "CANEVAS COSMIQUE",
         couleur: "purple",
         imgPresentation: "images/projet03_canevas-cosmique.png",
-        description: "Vous vous retrouvez dans un laboratoire contenant une table, après un peu de recherche vous découvrez que sur l'écran au dessus de celle-ci se trouve une simulation d'un système solaire avec laquelle vous pouvez intéragir en déposant des statues déclancheant différents phénomènes. Vous êtes invité à expérimenter sur cette table, jusqu'à ce que les scientifiques reviennent du moins...",
+        description: "Vous vous retrouvez dans un laboratoire contenant une table, après un peu de recherche vous découvrez que sur l'écran au-dessus de celle-ci se trouve une simulation d'un système solaire avec laquelle vous pouvez interagir en déposant des statues déclenchant différents phénomènes. Vous êtes invités à expérimenter sur cette table jusqu'à ce que les scientifiques reviennent, du moins... Dans cette installation intéractive, j'ai participé à la programmation de l'oeuvre dans Unity, l'installation physique de l'oeuvre et les montages vidéos",
         logiciels: ["Unity", "Touch Designer", "Davinci Resolve", "Maya/Blender", "Repear", "Figma", "OBS"],
         iframe: [`<iframe width="560" height="315" src="https://www.youtube.com/embed/Jrn4U27G5YY?si=GXibq9jUBFFprD-p" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`, `<iframe width="560" height="315" src="https://www.youtube.com/embed/5xu6JZipWec?si=-xqcfMbwkH0TxAjE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`],
         images: [],
     }
 ]
+function checkDesactiver(balise){
+    let variableTrouver = false;
+    balise.classList.forEach((c)=>{
+        if(c == "desactiver"){
+            variableTrouver = true;
+        }
+    })
+    return variableTrouver;
+    } 
 let carousselOeuvresSlides = document.querySelector(".swiperOeuvres .swiper-wrapper");
 let imgOeuvreSelectionner = document.querySelector(".oeuvreWrapup__img");
 let titreOeuvre = document.querySelector(".titreOeuvre")
@@ -123,13 +183,46 @@ let contenus = {
         titre: "PROJETS",
     }
 }
-
 /**** Bouton projet (CHANGE LE CONTENUS DE LA PAGE POUR LA PAGE PROJET)*****/
-let animationTitrePrete = false;
 let contenusDesactiver = false;
 let animationTerminer = false;
-btnProjets.addEventListener("click", function () {
-    animationTitrePrete = true;
+function pageVersAccueil(){
+    console.log("test");
+    if(contenus.accueil.active == false){
+        console.log("partie 1")
+        contenus.accueil.active = true;
+        contenus.projets.active = false;
+        if(checkDesactiver(carousselOeuvres) == false){
+            carousselOeuvres.classList.add("desactiver");
+            swiperOeuvres.slideTo(0);
+        }
+        gsap.to(".animationContenus", {
+            y: "-110%",
+            duration: 0.3,
+        })
+        setTimeout(() => {
+
+            titre.innerText = contenus.accueil.titre;
+            btnProjets.classList.remove("desactiver");
+            if(checkDesactiver(imgScroll) == false){
+                imgScroll.classList.add("desactiver");
+            }
+            if(checkDesactiver(imgSwipe) == false){
+                imgSwipe.classList.add("desactiver");
+            }
+            gsap.fromTo(".animationContenus",
+                { y: "100%" },
+                {
+                    y: 0,
+                    duration: 0.8,
+                    ease: "back.out"
+                }
+            )
+
+            }, 300);
+    }
+}
+function pageVersProjets(){
     if (contenus.projets.active == false) {
         gsap.to(".animationContenus", {
             y: "-100%",
@@ -200,45 +293,11 @@ btnProjets.addEventListener("click", function () {
         }, 300);
 
     };
-});
+}
+btnProjets.addEventListener("click", pageVersProjets);
+btnPortfolio.addEventListener("click",pageVersProjets);
+btnNom.addEventListener("click", pageVersAccueil);
 
-/******* swiper pour les oeuvres**********/
-const swiperOeuvres = new Swiper(".swiperOeuvres", {
-    slidesPerView: 1,
-    breakpoints: {
-        1300: {
-            slidesPerView: 3,
-            mousewheel: {
-                enabled: true,
-                sensitivity: 5,
-            },
-        },
-        830: {
-            slidesPerView: 2,
-            mousewheel: {
-                enabled: true,
-                sensitivity: 3,
-            },
-        },
-        550: {
-            slidesPerView: 1,
-            mousewheel: {
-                enabled: true,
-                sensitivity: 2,
-            },
-        }
-
-    },
-    freeMode: {
-        enabled: true,
-        sticky: false,
-        momentumBounce: false,
-    },
-    mousewheel: {
-        enabled: true,
-        sensitivity: 1,
-    },
-})
 
 
 let swiperActif = false;
@@ -265,16 +324,6 @@ swiperOeuvres.on("slideChange", function () {
         });
     }
 });
-
-
-/********swiper pour les images de l'oeuvre**********/
-const swiperImages = new Swiper(".swiperImages", {
-    slidesPerView: 1,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-})
 
 
 /***********Ferme Oeuvre ouvert**********/
